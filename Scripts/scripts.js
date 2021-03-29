7d5268caead45ec06be5e71a3ce96998664aa897
Kyle <kylemguy24@gmail.com>
2021-03-29 18:04:21 -0400

    Making it pretty

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -9,6 +9,7 @@ const weatherDisplay = document.getElementById("weather");
 const displaySection = document.getElementById("displaySection");
 const descriptionSection = document.getElementById("description"); 
 
+
 //API Key
 const apiKey = "858acac015592bf4cd9498de977d8cc6"; 
 
@@ -28,16 +29,28 @@ function getWeather(city){
     //once it receives the response its converted to JSON and the passed into the display function
     fetch(api)
         .then(response => response.json())
-        .then(data => displayWeather(data));
+        .then(data => displayWeather(data))
+        .catch(e => {
+            displayError(); 
+        })
 }
 
 //display weather takes the response from the fetch and displays the city name, temperature, and
 //the icon that openweathermap gives for each weather type
 function displayWeather(response){
-    displaySection.style.visibility = 'visible'; 
+    displaySection.style.visibility = 'visible';
     cityDisplay.innerText = response.name;
     tempDisplay.innerHTML = `${Math.floor(response.main.temp)} °F`;
     descriptionSection.innerText = response.weather[0].description; 
     weatherDisplay.innerHTML = `<img src = "http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png">`;
 
+}
+
+//this is the function that displays the error message if there was an issue finding the city 
+function displayError(){
+    displaySection.style.visibility = 'visible'; 
+    cityDisplay.innerText = "Sorry we couldn't find that city"; 
+    tempDisplay.style.visibility = 'hidden'; 
+    descriptionSection.style.visibility = 'hidden';
+    weatherDisplay.style.visibility = 'hidden'; 
 }
